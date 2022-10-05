2913f29fd70c07007c37bbeef7aebfdb2860e550
googlekeep <info.workingforyou@gmail.com>
2022-10-05 21:04:59 +0530

    hello js

---
+++ let-const.js
@@ -0,0 +1,20 @@
+// breakup with var
+// no more use of var
+// let it to reasion 
+// do not allow  it to reassigon
+
+
+let money = 25;
+money = 35;
+console.log(money);
+
+const bird = 'jann pakhi';
+// bird = 'money pakhi';
+const message = bird + 'potas potas'
+console.log(message);
+const numbers = [12, 89, 65, 45];
+// reassign is not allowed 
+// numbers = [77, 66, 55, 22]
+numbers.push(123);
+numbers[1]=90;
+console.log(numbers);
