d5e85268a7753eede6ceb5085ae02b87e076e117
googlekeep <info.workingforyou@gmail.com>
2022-10-10 08:44:29 +0530

    arrow

---
+++ function.js
@@ -0,0 +1,13 @@
+// function declaraton
+function add (first, second){
+    const total = first + second;
+    return total;
+} 
+
+const add1 = function add1(first,second){
+    const total = first + second;
+    return total;
+}
+
+const result = add(10, 20);
+console.log(result);
---
+++ more-arrow.js
@@ -0,0 +1,22 @@
+const add = (first, second) => first + second;
+const getFullName = (first, last) => first + '' + last;
+const multiply = (a, b) => a * b;
+
+
+const result = multiply(7, 8);
+
+
+
+const addAll = (a, b, c, d, e, f) => a+b+c+d+e+f;
+const getPie = () => 3.14;
+
+
+const fiveTime = num => num * 5;
+
+
+
+const doMath = (x, y, z) => {
+    const firstSum = x + y;
+    const secondSum = y = z;
+
+}
